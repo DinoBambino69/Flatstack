ff185a64f2b4facefeb1ec3072c8b0185d8ee802
Max <maximum_sill@mail.ru>
2019-04-22 21:10:57 +0300

    HW

---
+++ Encryption.rb
@@ -0,0 +1,18 @@
+require 'openssl'
+
+class Encryption
+  cipher = OpenSSL::Cipher.new('aes-256-cbc')
+  cipher.encrypt
+  key = cipher.random_key
+  iv = cipher.random_iv
+
+  buf = ""
+  File.open("file.enc", "wb") do |outf|
+    File.open("file", "rb") do |inf|
+      while inf.read(4096, buf)
+        outf << cipher.update(buf)
+      end
+      outf << cipher.final
+    end
+  end
+end
